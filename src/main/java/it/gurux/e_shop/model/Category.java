1cdfcc9ba5237a38efc2b7b613af03dffc77497f
DjGuruge <djguruge@hotmail.it>
2025-07-15 23:52:17 +0200

    added yml and first 3 entities

--- src/main/java/it/gurux/e_shop/model/Category.java
+++ src/main/java/it/gurux/e_shop/model/Category.java
@@ -1,11 +1,27 @@
 package it.gurux.e_shop.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
-    private List<Product> products;
+    @OneToMany(mappedBy = "category")
+    private List<Product> product;
 
 }

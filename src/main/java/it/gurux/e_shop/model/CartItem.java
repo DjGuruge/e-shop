99c58f0699025c073d762e96d6052c93a2892eef
DjGuruge <djguruge@hotmail.it>
2025-09-25 23:50:18 +0200

    added attributes to CartItem class

--- src/main/java/it/gurux/e_shop/model/CartItem.java
+++ src/main/java/it/gurux/e_shop/model/CartItem.java
@@ -1,4 +1,12 @@
 package it.gurux.e_shop.model;
 
+import java.math.BigDecimal;
+
 public class CartItem {
+    private Long id;
+    private Product product;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalprice;
+    private Cart cart;
 }

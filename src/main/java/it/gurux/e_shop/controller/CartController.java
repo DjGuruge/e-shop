d4edca3ad21a4a7e577d69c129f73d181a37192c
DjGuruge <djguruge@hotmail.it>
2025-09-28 17:14:18 +0200

    Created CartController.java added endpoint for getCart using id as path variable.

---
+++ src/main/java/it/gurux/e_shop/controller/CartController.java
@@ -0,0 +1,34 @@
+package it.gurux.e_shop.controller;
+
+
+import it.gurux.e_shop.exception.ResourceNotFoundException;
+import it.gurux.e_shop.model.Cart;
+import it.gurux.e_shop.response.ApiResponse;
+import it.gurux.e_shop.service.cart.CartService;
+import it.gurux.e_shop.service.cart.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("{$api.prefix}/cart")
+public class CartController {
+
+    private final ICartService cartService;
+
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId){
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Success", cart));
+        }catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+    }
+
+
+}

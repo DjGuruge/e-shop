b2b313e78e1c8dd2956b12956db23521adc03ee3
DjGuruge <djguruge@hotmail.it>
2025-11-06 10:35:24 +0100

    exixstByEmail method implemented in user repo

--- src/main/java/it/gurux/e_shop/repository/UserRepository.java
+++ src/main/java/it/gurux/e_shop/repository/UserRepository.java
@@ -4,4 +4,5 @@ import it.gurux.e_shop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
 }

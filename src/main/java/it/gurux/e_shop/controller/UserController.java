649f7bb7e926a15e18cdc4ebe4a3a88fd182169b
DjGuruge <djguruge@hotmail.it>
2025-11-10 12:22:47 +0100

    created getUserById method

--- src/main/java/it/gurux/e_shop/controller/UserController.java
+++ src/main/java/it/gurux/e_shop/controller/UserController.java
@@ -1,8 +1,17 @@
 package it.gurux.e_shop.controller;
 
+import it.gurux.e_shop.exception.ResourceNotFoundException;
+import it.gurux.e_shop.model.User;
+import it.gurux.e_shop.response.ApiResponse;
+import it.gurux.e_shop.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RequiredArgsConstructor
 @RestController
@@ -10,5 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
+    private final IUserService userService;
+
+
+    @GetMapping("/{userId}/user")
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId){
+        try {
+            User user = userService.getUserById(userId);
+            return ResponseEntity.ok(new ApiResponse("User found",user));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));        }
+    }
 
 }

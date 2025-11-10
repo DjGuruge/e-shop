24c5a4b76f32291fccca7034a36af0fca28188f3
DjGuruge <djguruge@hotmail.it>
2025-11-10 12:07:12 +0100

    implementing oderDto

--- src/main/java/it/gurux/e_shop/service/order/IOrderService.java
+++ src/main/java/it/gurux/e_shop/service/order/IOrderService.java
@@ -1,12 +1,13 @@
 package it.gurux.e_shop.service.order;
 
+import it.gurux.e_shop.dto.OrderDto;
 import it.gurux.e_shop.model.Order;
 
 import java.util.List;
 
 public interface IOrderService {
-    Order placeOrder(Long userdId);
-    Order getOrder(Long orderId);
+    Order placeOrder(Long userId);
+    OrderDto getOrder(Long orderId);
 
-    List<Order> getUserOrders(Long userId);
+    List<OrderDto> getUserOrders(Long userId);
 }

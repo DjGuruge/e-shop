34a03f2d9c1878d33df23194ce93a85e9a679be8
Vincent Henry Guruge <djguruge@hotmail.it>
2026-07-30 02:10:40 +0200

    learning to implement java security,implementing shoperuserdetails

--- src/main/java/it/gurux/e_shop/service/ShopUserDetails.java
+++ src/main/java/it/gurux/e_shop/service/ShopUserDetails.java
@@ -1,12 +1,52 @@
 package it.gurux.e_shop.service;
 
+import it.gurux.e_shop.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.stream;
+import static sun.awt.image.MultiResolutionCachedImage.map;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShopUserDetails implements UserDetails {
+
+    private Long  id;
+    private String username;
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+
+
+    public static ShopUserDetails buildUserDetail(User user) {
+        List<GrantedAuthority> authorities = user.getRoles()Collection<Role>
+        .stream() Stream<Role>
+                .map(role-> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new ShopUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+
+    }
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();

edd61c8abf43a0a0a5f4885bd5f6bc512d28f096
Vincent Henry Guruge <djguruge@hotmail.it>
2026-08-20 10:32:42 +0200

    created the role repo.

---
+++ src/main/java/it/gurux/e_shop/data/RoleRepository.java
@@ -0,0 +1,12 @@
+package it.gurux.e_shop.data;
+
+import it.gurux.e_shop.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String role);
+
+
+}
